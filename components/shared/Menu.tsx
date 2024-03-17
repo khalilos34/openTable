381fcee00f7644ad281f231ba3c58a29d75fb51d
khalilos34 <khalil.naoueji@gmail.com>
2024-03-17 16:44:54 +0100

    fix some mistakes

--- components/shared/Menu.tsx
+++ components/shared/Menu.tsx
@@ -1,21 +1,23 @@
 import { Item } from "@prisma/client";
 import MenuCard from "./MenuCard";
+import { MdMenuBook } from "react-icons/md";
 
 const Menu = ({ menu }: { menu: Item[] }) => {
   return (
     <main className="bg-white mt-5">
-      <div>
-        <div className="mt-4 pb-1 mb-1">
-          <h1 className="font-bold text-4xl">Menu</h1>
+      <div className="mt-4 pb-1 mb-1">
+        <div className="flex items-start justify-center gap-x-4">
+          <h1 className="font-bold text-center text-4xl mb-8">Menu</h1>
+          <MdMenuBook size={30} />
+        </div>
 
-          <div className="flex flex-wrap justify-between">
-            {menu &&
-              menu.map((item) => (
-                <div key={item.id}>
-                  <MenuCard item={item} />
-                </div>
-              ))}
-          </div>
+        <div className="flex flex-col  ">
+          {menu &&
+            menu.map((item) => (
+              <div key={item.id}>
+                <MenuCard item={item} />
+              </div>
+            ))}
         </div>
       </div>
     </main>
